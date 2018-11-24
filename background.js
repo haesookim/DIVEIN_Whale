ee812f38b8eee31ab76a7c12f960d91587be29d5
Yeong-Rok <yeongrok.jeong@gmail.com>
2018-11-25 04:17:53 +0900

    show the static list of tabs on the sidebar

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
+const list = document.querySelector(`#tablist`);
+
+// get tabs of current window
+whale.tabs.query({'currentWindow': true}, function (tabtree) {
+  tabtree.forEach(function (tab) {
+    listingTabs(tab);
+  });
+});
+
 // Tab is created
 whale.tabs.onCreated.addListener(() => {
   // create tree node and add to tree
@@ -8,3 +17,9 @@ whale.tabs.onUpdated.addListener(() => {
   // update every time link is changed)
   // might update with every reload - might not matter if tree hierarchy is maintained
 });
+
+// list the titles of currently opened tabs in "sidebar.html"
+function listingTabs(tab) {
+  console.log(list); // null <- unsolved error!
+  list.innerHTML += `<li>${tab.title}</li>`;
+}
